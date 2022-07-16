6b4bd6f8046b5873c730e038b523437b98fe3a55
tuyennx <xuantuyen.nguyen@vmodev.com>
2022-07-16 19:11:26 +0700

    first commit

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,10 @@ const axios = require('axios');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  const result = await axios.get('http://13.229.57.126:3000/users');
+  const result = await axios.get('http://ip_server:3000/users');
 
   console.log(434343, result.data.data);
-  
+
   res.render('index', { data: result.data.data });
 });
 
