fa2389f7d36f15feda4c1953d918a36b70df8c86
tuyennx <xuantuyen.nguyen@vmodev.com>
2022-09-13 11:06:29 +0700

    test

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var router = express.Router();
 const axios = require('axios');
+const os = require('os');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  const result = await axios.get('http://ip_server:3000/users');
+  const result = await axios.get('https://zipcloud.ibsnet.co.jp/api/search?zipcode=2700014');
 
-  console.log(434343, result.data.data);
-
-  res.render('index', { data: result.data.data });
+  // console.log(434343, os.hostname());
+  res.json(result.data)
+  // res.render('index', { data: result.data });
 });
 
 module.exports = router;
